fe0eac15f0937495070f72012648a2348789c7a5
xxg <XXGGG@users.noreply.github.com>
2026-01-26 20:15:04 +0800

    ✨ feat: 暗黑模式

--- src/components/settings/ModeToggle.vue
+++ src/components/settings/ModeToggle.vue
@@ -1,6 +1,13 @@
 <script setup lang="ts">
+import { useDark, useToggle } from '@vueuse/core'
+import { Button } from '@/components/ui/button'
+
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
 </script>
 
 <template>
-   <div></div>
+   <Button variant="outline" size="icon" @click="toggleDark()">
+      <span :class="isDark ? 'icon-[lucide--moon]' : 'icon-[lucide--sun-dim]'" class="w-5 h-5"></span>
+   </Button>
 </template>
